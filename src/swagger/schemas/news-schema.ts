3eee260b4bffd8067dc992a77247935b5873a4a5
matheusjean <matheusjean11@hotmail.com>
2023-10-10 18:29:25 -0300

    update getNewsIdSchema

--- src/swagger/schemas/news-schema.ts
+++ src/swagger/schemas/news-schema.ts
@@ -1,36 +1,49 @@
-
 export const GetIdUseCase = {
-  type: 'array',
-  items: {
-    type: 'object',
-    properties: {
-      id: {
-        type: 'string',
-      },
-      title: {
-        type: 'string',
-      },
-      hat: {
-        type: 'string',
-      },
-      text: {
-        type: 'string',
-      },
-      author: {
-        type: 'string',
-      },
-      image: {
-        type: 'string',
-      },
-      link: {
-        type: 'string',
-      },
-      isActive: {
-        type: 'boolean',
-      },
-      created_at: {
-        type: 'date',
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string',
+    },
+    title: {
+      type: 'string',
+    },
+    hat: {
+      type: 'string',
+    },
+    text: {
+      type: 'string',
+    },
+    author: {
+      type: 'string',
+    },
+    image: {
+      type: 'string',
+    },
+    link: {
+      type: 'string',
+    },
+    isActive: {
+      type: 'boolean',
+    },
+    created_at: {
+      type: 'date',
+    },
+    categories: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+          },
+          name: {
+            type: 'string',
+          },
+          isActive: {
+            type: 'boolean',
+          },
+        },
       },
     },
-    },
+  },
 };
