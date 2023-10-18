786bf8c837606336fad0da4e6f89666f36b6599c
matheusjean <matheusjean11@hotmail.com>
2023-10-18 19:21:06 -0300

    feat: create session service

---
+++ src/modules/users/service/CreateSessionService.ts
@@ -0,0 +1,51 @@
+import { PrismaClient, User } from '@prisma/client/edge';
+import AppError from '@shared/errors/appError';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import authConfig from '@config/middleware/authConfig';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: User;
+  token: string;
+}
+
+const prisma = new PrismaClient();
+
+class CreateSessionsService {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new AppError('E-mail ou senha incorretos!', 401);
+    }
+
+    const passwordConfirmed = await compare(password, user.password);
+
+    if (!passwordConfirmed) {
+      throw new AppError('E-mail ou senha incorretos!', 401);
+    }
+
+    const token = sign(
+      { id: user.id, username: user.username },
+      authConfig.jwt.secret,
+      {
+        subject: user.id,
+        expiresIn: authConfig.jwt.expiresIn,
+      },
+    );
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default CreateSessionsService;
