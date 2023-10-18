b19e2461a98b8b74d3aa1d723836546c972abb4a
matheusjean <matheusjean11@hotmail.com>
2023-10-18 19:35:06 -0300

    feat: /login and /users rountes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,14 @@
 import categoryRouter from '../../../modules/category/routes/Category.routes';
 import newsRouter from '../../../modules/news/routes/News.routes';
 import { Router } from 'express';
+import sessionRouter from '@modules/users/routes/Sessions.routes';
+import usersRouter from '@modules/users/routes/Users.routes';
 
 const routes = Router();
+
+routes.use('/login', sessionRouter);
+
+routes.use('/users', usersRouter);
 
 routes.use('/news', newsRouter);
 
